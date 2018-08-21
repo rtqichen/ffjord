234a50f4ff88a9403d2b23f9afb0354650a562df
Ricky Chen <rtqichen@gmail.com>
2018-08-20 22:09:38 -0400

    default tanh, -rademacher

--- train_toy.py
+++ train_toy.py
@@ -32,7 +32,7 @@ parser.add_argument("--num_blocks", type=int, default=1, help='Number of stacked
 parser.add_argument('--time_length', type=float, default=0.5)
 parser.add_argument('--train_T', type=eval, default=True)
 parser.add_argument("--divergence_fn", type=str, default="approximate", choices=["brute_force", "approximate"])
-parser.add_argument("--nonlinearity", type=str, default="softplus", choices=["tanh", "relu", "softplus", "elu"])
+parser.add_argument("--nonlinearity", type=str, default="tanh", choices=["tanh", "relu", "softplus", "elu"])
 
 parser.add_argument('--solver', type=str, default='dopri5', choices=SOLVERS)
 parser.add_argument('--atol', type=float, default=1e-5)
@@ -44,7 +44,7 @@ parser.add_argument('--test_atol', type=float, default=None)
 parser.add_argument('--test_rtol', type=float, default=None)
 
 parser.add_argument('--residual', type=eval, default=False, choices=[True, False])
-parser.add_argument('--rademacher', type=eval, default=True, choices=[True, False])
+parser.add_argument('--rademacher', type=eval, default=False, choices=[True, False])
 parser.add_argument('--spectral_norm', type=eval, default=False, choices=[True, False])
 parser.add_argument('--batch_norm', type=eval, default=False, choices=[True, False])
 parser.add_argument('--bn_lag', type=float, default=0)
