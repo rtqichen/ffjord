db044e334cb118ab65d00c69fa439cfad5614902
Ricky Chen <rtqichen@gmail.com>
2018-09-18 22:26:27 -0400

    add plotting scripts

---
+++ diagnostics/plot_bottleneck_losses.py
@@ -0,0 +1,64 @@
+import re
+import numpy as np
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+import scipy.signal
+import scipy.ndimage
+
+# BASE = "experiments/cnf_mnist_64-64-128-128-64-64/logs"
+# RESIDUAL = "experiments/cnf_mnist_64-64-128-128-64-64_residual/logs"
+# RADEMACHER = "experiments/cnf_mnist_64-64-128-128-64-64_rademacher/logs"
+
+BOTTLENECK = "experiments/cnf_mnist_bottleneck_64-64-128-5-128-64-64/logs"
+BOTTLENECK_EST = "experiments/cnf_mnist_bottleneck_64-64-128-5-128-64-64_ae-est/logs"
+RAD_BOTTLENECK = "experiments/cnf_mnist_bottleneck_64-64-128-5-128-64-64_rademacher/logs"
+RAD_BOTTLENECK_EST = "experiments/cnf_mnist_bottleneck_64-64-128-5-128-64-64_ae-est_rademacher/logs"
+
+# ET_ALL = "experiments/cnf_mnist_bottleneck_64-64-128-5-128-64-64_ae-est_residual_rademacher/logs"
+
+
+def get_losses(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    losses = []
+
+    for line in lines:
+        w = re.findall(r"Bit/dim [^|(]*\([0-9\.]*\)", line)
+        if w: w = re.findall(r"\([0-9\.]*\)", w[0])
+        if w: w = re.findall(r"[0-9\.]+", w[0])
+        if w:
+            losses.append(float(w[0]))
+
+    return losses
+
+
+bottleneck_loss = get_losses(BOTTLENECK)
+bottleneck_est_loss = get_losses(BOTTLENECK_EST)
+rademacher_bottleneck_loss = get_losses(RAD_BOTTLENECK)
+rademacher_bottleneck_est_loss = get_losses(RAD_BOTTLENECK_EST)
+
+bottleneck_loss = scipy.signal.medfilt(bottleneck_loss, 21)
+bottleneck_est_loss = scipy.signal.medfilt(bottleneck_est_loss, 21)
+rademacher_bottleneck_loss = scipy.signal.medfilt(rademacher_bottleneck_loss, 21)
+rademacher_bottleneck_est_loss = scipy.signal.medfilt(rademacher_bottleneck_est_loss, 21)
+
+import brewer2mpl
+line_colors = brewer2mpl.get_map('Set2', 'qualitative', 4).mpl_colors
+dark_colors = brewer2mpl.get_map('Dark2', 'qualitative', 4).mpl_colors
+plt.style.use('ggplot')
+
+plt.figure(figsize=(4, 2.5))
+plt.plot(np.arange(len(bottleneck_loss)) / 30, bottleneck_loss, color=line_colors[1], label="Gaussian w/o Bottleneck Trick")
+plt.plot(np.arange(len(bottleneck_est_loss)) / 30, bottleneck_est_loss, color=dark_colors[1], label="Gaussian w/ Bottleneck Trick")
+plt.plot(np.arange(len(rademacher_bottleneck_loss)) / 30, rademacher_bottleneck_loss, color=line_colors[2], label="Rademacher w/o Bottleneck Trick")
+plt.plot(np.arange(len(rademacher_bottleneck_est_loss)) / 30, rademacher_bottleneck_est_loss, color=dark_colors[2], label="Rademacher w/ Bottleneck Trick")
+
+plt.legend(fontsize=7)
+plt.ylim([1, 1.5])
+# plt.yscale("log", nonposy='clip')
+plt.xlabel("Epoch", fontsize=14)
+plt.ylabel("Bits/dim", fontsize=14)
+plt.tight_layout()
+plt.savefig('bottleneck_losses.pdf')
---
+++ diagnostics/plot_sn_losses.py
@@ -0,0 +1,81 @@
+import re
+import matplotlib
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
+
+CIFAR10 = "diagnostics/cifar10.log"
+CIFAR10_SN = "diagnostics/cifar10_sn.log"
+
+MNIST = "diagnostics/mnist.log"
+MNIST_SN = "diagnostics/mnist_sn.log"
+
+
+def get_values(filename):
+    with open(filename, "r") as f:
+        lines = f.readlines()
+
+    losses = []
+    nfes = []
+
+    for line in lines:
+
+        w = re.findall(r"Steps [^|(]*\([0-9\.]*\)", line)
+        if w: w = re.findall(r"\([0-9\.]*\)", w[0])
+        if w: w = re.findall(r"[0-9\.]+", w[0])
+        if w:
+            nfes.append(float(w[0]))
+
+            w = re.findall(r"Bit/dim [^|(]*\([0-9\.]*\)", line)
+            if w: w = re.findall(r"\([0-9\.]*\)", w[0])
+            if w: w = re.findall(r"[0-9\.]+", w[0])
+            if w:
+                losses.append(float(w[0]))
+
+    return losses, nfes
+
+
+cifar10_loss, cifar10_nfes = get_values(CIFAR10)
+cifar10_sn_loss, cifar10_sn_nfes = get_values(CIFAR10_SN)
+mnist_loss, mnist_nfes = get_values(MNIST)
+mnist_sn_loss, mnist_sn_nfes = get_values(MNIST_SN)
+
+import brewer2mpl
+line_colors = brewer2mpl.get_map('Set2', 'qualitative', 4).mpl_colors
+dark_colors = brewer2mpl.get_map('Dark2', 'qualitative', 4).mpl_colors
+plt.style.use('ggplot')
+
+# CIFAR10 plot
+plt.figure(figsize=(6, 7))
+plt.scatter(cifar10_nfes, cifar10_loss, color=line_colors[1], label="w/o Spectral Norm")
+plt.scatter(cifar10_sn_nfes, cifar10_sn_loss, color=line_colors[2], label="w/ Spectral Norm")
+
+plt.ylim([3, 5])
+plt.legend(fontsize=18)
+plt.xlabel("NFE", fontsize=30)
+plt.ylabel("Bits/dim", fontsize=30)
+
+ax = plt.gca()
+ax.tick_params(axis='both', which='major', labelsize=24)
+ax.tick_params(axis='both', which='minor', labelsize=16)
+ax.yaxis.set_ticks([3, 3.5, 4, 4.5, 5])
+
+plt.tight_layout()
+plt.savefig('cifar10_sn_loss_vs_nfe.pdf')
+
+# MNIST plot
+plt.figure(figsize=(6, 7))
+plt.scatter(mnist_nfes, mnist_loss, color=line_colors[1], label="w/o Spectral Norm")
+plt.scatter(mnist_sn_nfes, mnist_sn_loss, color=line_colors[2], label="w/ Spectral Norm")
+
+plt.ylim([0.9, 2])
+plt.legend(fontsize=18)
+plt.xlabel("NFE", fontsize=30)
+plt.ylabel("Bits/dim", fontsize=30)
+
+ax = plt.gca()
+ax.tick_params(axis='both', which='major', labelsize=24)
+ax.tick_params(axis='both', which='minor', labelsize=16)
+# ax.yaxis.set_ticks([3, 3.5, 4, 4.5, 5])
+
+plt.tight_layout()
+plt.savefig('mnist_sn_loss_vs_nfe.pdf')
