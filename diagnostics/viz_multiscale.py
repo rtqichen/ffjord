474886343af16c2cb80e937a651ffd7829582d83
Ricky Chen <rtqichen@gmail.com>
2018-10-03 12:03:08 -0400

    use z interpolation

--- diagnostics/viz_multiscale.py
+++ diagnostics/viz_multiscale.py
@@ -25,15 +25,12 @@ parser.add_argument("--checkpt", type=str, required=True)
 parser.add_argument("--data", choices=["mnist", "svhn", "cifar10", 'lsun_church'], type=str, default="cifar10")
 parser.add_argument("--dims", type=str, default="64,64,64")
 parser.add_argument("--num_blocks", type=int, default=2, help='Number of stacked CNFs.')
-parser.add_argument("--conv", type=eval, default=True, choices=[True, False])
-parser.add_argument(
-    "--layer_type", type=str, default="ignore",
-    choices=["ignore", "concat", "concat_v2", "squash", "concatsquash", "concatcoord", "hyper", "blend"]
-)
 parser.add_argument("--divergence_fn", type=str, default="approximate", choices=["brute_force", "approximate"])
 parser.add_argument(
     "--nonlinearity", type=str, default="softplus", choices=["tanh", "relu", "softplus", "elu", "swish"]
 )
+parser.add_argument("--conv", type=eval, default=True, choices=[True, False])
+
 parser.add_argument('--solver', type=str, default='dopri5')
 parser.add_argument('--atol', type=float, default=1e-5)
 parser.add_argument('--rtol', type=float, default=1e-5)
@@ -44,7 +41,7 @@ parser.add_argument('--test_atol', type=float, default=None)
 parser.add_argument('--test_rtol', type=float, default=None)
 
 parser.add_argument("--imagesize", type=int, default=None)
-parser.add_argument("--alpha", type=float, default=0)
+parser.add_argument("--alpha", type=float, default=-1.0)
 parser.add_argument('--time_length', type=float, default=1.0)
 parser.add_argument('--train_T', type=eval, default=True)
 
@@ -57,6 +54,8 @@ parser.add_argument('--ntimes', type=int, default=50)
 parser.add_argument('--save', type=str, default='img_trajectory')
 
 args = parser.parse_args()
+
+BATCH_SIZE = 100
 
 
 def add_noise(x):
@@ -111,7 +110,7 @@ def create_model(args, data_shape):
     hidden_dims = tuple(map(int, args.dims.split(",")))
 
     model = odenvp.ODENVP(
-        (100, *data_shape),
+        (BATCH_SIZE, *data_shape),
         n_blocks=args.num_blocks,
         intermediate_dims=hidden_dims,
         nonlinearity=args.nonlinearity,
@@ -122,8 +121,6 @@ def create_model(args, data_shape):
     set_cnf_options(args, model)
     return model
 
-
-BATCH_SIZE = 100
 
 if __name__ == '__main__':
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
@@ -149,7 +146,19 @@ if __name__ == '__main__':
 
     data_samples, _ = train_loader.__iter__().__next__()
 
-    z = data_samples
+    # cosine interpolate between 4 real images.
+    z = data_samples[:4]
+    print('Inferring base values for 4 example images.')
+    z = model(z)
+
+    phi0 = torch.linspace(0, 0.5, int(math.sqrt(BATCH_SIZE))) * math.pi
+    phi1 = torch.linspace(0, 0.5, int(math.sqrt(BATCH_SIZE))) * math.pi
+    phi0, phi1 = torch.meshgrid([phi0, phi1])
+    phi0, phi1 = phi0.contiguous().view(-1, 1), phi1.contiguous().view(-1, 1)
+    z = torch.cos(phi0) * (torch.cos(phi1) * z[0] + torch.sin(phi1) * z[1]) + \
+        torch.sin(phi0) * (torch.cos(phi1) * z[2] + torch.sin(phi1) * z[3])
+    print('Reconstructing images from latent interpolation.')
+    z = model(z, reverse=True)
 
     non_cnf_layers = []
 
@@ -176,6 +185,8 @@ if __name__ == '__main__':
             T = x.shape[0] // self.factor_out.shape[0]
             return torch.cat([x, self.factor_out.repeat(T, *([1] * (self.factor_out.ndimension() - 1)))], 1)
 
+    time_ratio = 1.0
+    print('Visualizing transformations.')
     with torch.no_grad():
         for idx, stacked_layers in enumerate(model.transforms):
             for layer in stacked_layers.chain:
@@ -185,14 +196,15 @@ if __name__ == '__main__':
                     # linspace over time, and visualize by reversing through previous non_cnf_layers.
                     cnf = layer
                     end_time = (cnf.sqrt_end_time * cnf.sqrt_end_time)
-                    integration_times = torch.linspace(0, end_time.item(), args.ntimes)
+                    ntimes = int(args.ntimes * time_ratio)
+                    integration_times = torch.linspace(0, end_time.item(), ntimes)
                     z_traj = cnf(z, integration_times=integration_times)
 
                     # reverse z(t) for all times to the input space
-                    z_flatten = z_traj.view(args.ntimes * BATCH_SIZE, *z_traj.shape[2:])
+                    z_flatten = z_traj.view(ntimes * BATCH_SIZE, *z_traj.shape[2:])
                     for prev_layer in non_cnf_layers[::-1]:
                         z_flatten = prev_layer(z_flatten, reverse=True)
-                    z_inv = z_flatten.view(args.ntimes, BATCH_SIZE, *data_shape)
+                    z_inv = z_flatten.view(ntimes, BATCH_SIZE, *data_shape)
                     for t in range(1, z_inv.shape[0]):
                         z_t = z_inv[t]
                         save_imgs_figure(z_t)
@@ -205,3 +217,6 @@ if __name__ == '__main__':
                 d = z.shape[1] // 2
                 z, factor_out = z[:, :d], z[:, d:]
                 non_cnf_layers.append(FactorOut(factor_out))
+
+                # After every factor out, we half the time for visualization.
+                time_ratio = time_ratio / 2
